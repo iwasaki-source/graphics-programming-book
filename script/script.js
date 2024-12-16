8b661bac78f5f7f1248a9c56faf97530159b2bb1
iwasaki <poettype73@gmail.com>
2024-12-17 01:29:43 +0900

    add 001

---
+++ script/script.js
@@ -0,0 +1,37 @@
+(() => {
+  const CANVAS_WIDTH = 640;
+  const CANVAS_HEIGHT = 480;
+
+  let util = null;
+  let canvas = null;
+  let ctx = null;
+  let image = null;
+
+  window.addEventListener('load', () => {
+    util = new Canvas2DUtility(document.body.querySelector('#main_canvas'));
+    canvas = util.canvas;
+    ctx = util.context;
+
+    util.imageLoader('./image/viper.png', (loadImage) => {
+      image = loadImage;
+      initialize();
+      render();
+    })
+  });
+
+  function initialize() {
+    canvas.width = CANVAS_WIDTH;
+    canvas.height = CANVAS_HEIGHT;
+  }
+
+  function render() {
+    util.drawRect(0, 0, canvas.width, canvas.height, '#eeeeee');
+    ctx.drawImage(image, 100, 100);
+  }
+
+  function generateRandomInt(range) {
+    let random = Math.random();
+    return Math.floor(random * range);
+  }
+
+})();
