c27cb09fd663015a1a10b12292e493c9352964f8
iwasaki <poettype73@gmail.com>
2024-12-18 00:06:56 +0900

    add 002

--- script/script.js
+++ script/script.js
@@ -6,6 +6,7 @@
   let canvas = null;
   let ctx = null;
   let image = null;
+  let startTime = null;
 
   window.addEventListener('load', () => {
     util = new Canvas2DUtility(document.body.querySelector('#main_canvas'));
@@ -15,6 +16,7 @@
     util.imageLoader('./image/viper.png', (loadImage) => {
       image = loadImage;
       initialize();
+      startTime = Date.now();
       render();
     })
   });
@@ -26,7 +28,14 @@
 
   function render() {
     util.drawRect(0, 0, canvas.width, canvas.height, '#eeeeee');
-    ctx.drawImage(image, 100, 100);
+
+    let nowTime = (Date.now() - startTime) / 1000;
+    let s = Math.sin(nowTime);
+    let x = s * 100.0;
+
+    ctx.drawImage(image, CANVAS_WIDTH / 2 + x, CANVAS_HEIGHT / 2);
+
+    requestAnimationFrame(render);
   }
 
   function generateRandomInt(range) {
