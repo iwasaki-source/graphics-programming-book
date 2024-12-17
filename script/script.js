2ab8a27e39121ae23f50d2af3b84c3281501c628
iwasaki <poettype73@gmail.com>
2024-12-18 00:53:12 +0900

    add 004

--- script/script.js
+++ script/script.js
@@ -11,6 +11,9 @@
   let viperX = CANVAS_WIDTH / 2;
   let viperY = CANVAS_HEIGHT / 2;
 
+  let isComing = false;
+  let comingStart = null;
+
   window.addEventListener('load', () => {
     util = new Canvas2DUtility(document.body.querySelector('#main_canvas'));
     canvas = util.canvas;
@@ -28,10 +31,16 @@
   function initialize() {
     canvas.width = CANVAS_WIDTH;
     canvas.height = CANVAS_HEIGHT;
+
+    isComing = true;
+    comingStart = Date.now();
+    viperY = CANVAS_HEIGHT;
   }
 
   function eventSetting() {
     window.addEventListener('keydown', (event) => {
+      if (isComing === true) return;
+
       switch(event.key) {
         case 'ArrowLeft':
           viperX -= 10;
@@ -50,9 +59,26 @@
   }
 
   function render() {
+    ctx.globalAlpha = 1.0;
     util.drawRect(0, 0, canvas.width, canvas.height, '#eeeeee');
 
     let nowTime = (Date.now() - startTime) / 1000;
+
+    if (isComing === true) {
+      let justTime = Date.now();
+      let comingTime = (justTime - comingStart) / 1000;
+
+      viperY = CANVAS_HEIGHT - comingTime * 50;
+
+      if (viperY <= CANVAS_HEIGHT - 100) {
+        isComing = false;
+        viperY = CANVAS_HEIGHT - 100;
+      }
+
+      if (justTime % 100 < 50) {
+        ctx.globalAlpha = 0.5;
+      }
+    }
 
     ctx.drawImage(image, viperX, viperY);
 
