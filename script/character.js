ffc82602cfc243fa85d717aaaf2a394f9eb61fe4
iwasaki <poettype73@gmail.com>
2024-12-18 23:50:55 +0900

    add 006

--- script/character.js
+++ script/character.js
@@ -11,25 +11,32 @@ class Position {
 }
 
 class Character {
-  constructor(ctx, x, y, life, image) {
+  constructor(ctx, x, y, w, h, life, image) {
     this.ctx = ctx;
     this.position = new Position(x, y);
+    this.width = w;
+    this.height = h;
     this.life = life;
     this.image = image;
   }
 
   draw() {
+    let offsetX = this.width / 2;
+    let offsetY = this.height / 2;
+
     this.ctx.drawImage(
       this.image,
-      this.position.x,
-      this.position.y
+      this.position.x - offsetX,
+      this.position.y - offsetY,
+      this.width,
+      this.height
     );
   }
 }
 
 class Viper extends Character {
-  constructor(ctx, x, y, image) {
-    super(ctx, x, y, 0, image);
+  constructor(ctx, x, y, w, h, image) {
+    super(ctx, x, y, w, h, 0, image);
 
     this.isComing = false;
     this.comingStart = null;
@@ -40,6 +47,31 @@ class Viper extends Character {
     this.isComing = true;
     this.comingStart = Date.now();
     this.position.set(startX, startY);
+    this.comingStartPosition = new Position(startX, startY);
     this.comingEndPosition = new Position(endX, endY);
+  }
+
+  update() {
+    let justTime = Date.now();
+
+    if (this.isComing === true) {
+      let comingTime = (justTime - this.comingStart) / 1000;
+
+      let y = this.comingStartPosition.y - comingTime * 50;
+
+      if (y <= this.comingEndPosition.y) {
+        this.isComing = false;
+        y = this.comingEndPosition.y;
+      }
+
+      this.position.set(this.position.x, y);
+
+      if (justTime % 100 < 50) {
+        this.ctx.globalAlpha = 0.5;
+      }
+    }
+
+    this.draw();
+    this.ctx.globalAlpha = 1.0;
   }
 }
--- script/script.js
+++ script/script.js
@@ -33,11 +33,11 @@
     canvas.width = CANVAS_WIDTH;
     canvas.height = CANVAS_HEIGHT;
 
-    viper = new Viper(ctx, 0, 0, image);
+    viper = new Viper(ctx, 0, 0, 64, 64, image);
 
     viper.setComing(
       CANVAS_WIDTH / 2,
-      CANVAS_HEIGHT,
+      CANVAS_HEIGHT + 50,
       CANVAS_WIDTH / 2,
       CANVAS_HEIGHT - 100
     );
@@ -70,24 +70,8 @@
 
     let nowTime = (Date.now() - startTime) / 1000;
 
-    if (viper.isComing === true) {
-      let justTime = Date.now();
-      let comingTime = (justTime - viper.comingStart) / 1000;
+    viper.update();
 
-      let y = CANVAS_HEIGHT - comingTime * 50;
-
-      if (y <= viper.comingEndPosition.y) {
-        viper.isComing = false;
-        y = viper.comingEndPosition.y;
-      }
-
-      viper.position.set(viper.position.x, y);
-
-      if (justTime % 100 < 50) {
-        ctx.globalAlpha = 0.5;
-      }
-    }
-    viper.draw();
     requestAnimationFrame(render);
   }
 
