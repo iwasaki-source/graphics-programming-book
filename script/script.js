80282670353995516e8e409c398a30778acecd66
iwasaki <poettype73@gmail.com>
2024-12-20 01:09:11 +0900

    add 012

--- script/script.js
+++ script/script.js
@@ -3,14 +3,16 @@
 
   const CANVAS_WIDTH = 640;
   const CANVAS_HEIGHT = 480;
-
+  const ENEMY_MAX_COUNT = 10;
   const SHOT_MAX_COUNT = 10;
 
   let viper = null;
   let util = null;
   let canvas = null;
   let ctx = null;
+  let scene = null;
   let startTime = null;
+  let enemyArray = [];
   let shotArray = [];
   let singleShotArray = [];
 
@@ -24,8 +26,11 @@
   });
 
   function initialize() {
+    let i;
     canvas.width = CANVAS_WIDTH;
     canvas.height = CANVAS_HEIGHT;
+
+    scene = new SceneManager();
 
     viper = new Viper(ctx, 0, 0, 64, 64, './image/viper.png');
 
@@ -35,6 +40,10 @@
       CANVAS_WIDTH / 2,
       CANVAS_HEIGHT - 100
     );
+
+    for (i = 0; i < ENEMY_MAX_COUNT; ++i) {
+      enemyArray[i] = new Enemy(ctx, 0, 0, 48, 48, './image/enemy_small.png');
+    }
 
     for (let i = 0; i < SHOT_MAX_COUNT; ++i) {
       shotArray[i] = new Shot(ctx, 0, 0, 32, 32, './image/viper_shot.png');
@@ -49,6 +58,10 @@
     let ready = true;
     ready = ready && viper.ready;
 
+    enemyArray.map((v) => {
+      ready = ready && v.ready;
+    });
+
     shotArray.map((v) => {
       ready = ready && v.ready;
     });
@@ -59,6 +72,7 @@
 
     if (ready) {
       eventSetting();
+      sceneSetting();
       startTime = Date.now();
       render();
     } else {
@@ -76,13 +90,40 @@
     })
   }
 
+  function sceneSetting() {
+    scene.add('intro', (time) => {
+      if (time > 2.0) {
+        scene.use('invade');
+      }
+    });
+
+    scene.add('invade', (time) => {
+      if (scene.frame !== 0) return;
+
+      for (let i = 0; i < ENEMY_MAX_COUNT; ++i) {
+        if (enemyArray[i].life <= 0) {
+          let e = enemyArray[i];
+          e.set(CANVAS_WIDTH / 2, -e.height);
+          e.setVector(0.0, 1.0);
+          break;
+        }
+      }
+    });
+    scene.use('intro');
+  }
+
   function render() {
     ctx.globalAlpha = 1.0;
     util.drawRect(0, 0, canvas.width, canvas.height, '#eeeeee');
 
     let nowTime = (Date.now() - startTime) / 1000;
 
+    scene.update();
     viper.update();
+
+    enemyArray.map((v) => {
+      v.update();
+    });
 
     shotArray.map((v) => {
       v.update();
