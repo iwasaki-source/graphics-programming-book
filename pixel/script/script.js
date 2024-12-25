3b5ae1ca50a5856a2c0e8c88b1373991c29eba40
iwasaki <poettype73@gmail.com>
2024-12-26 00:02:48 +0900

    add 002

--- pixel/script/script.js
+++ pixel/script/script.js
@@ -5,7 +5,6 @@
   let image = null;
   const CANVAS_SIZE = 512;
 
-  console.log('a');
   window.addEventListener('load', () => {
     imageLoader('./image/sample.jpg', (loadedImage) => {
       image = loadedImage;
@@ -24,7 +23,28 @@
   function render() {
     ctx.drawImage(image, 0, 0);
     let imageData = ctx.getImageData(0, 0, CANVAS_SIZE, CANVAS_SIZE);
-    console.log(imageData);
+    let outputData = invertFilter(imageData);
+    ctx.putImageData(outputData, 0, 0);
+  }
+
+  // ネガポジ反転フィルター
+  function invertFilter(imageData) {
+    let width = imageData.width;
+    let height = imageData.height;
+    let data = imageData.data;
+
+    let out = ctx.createImageData(width, height);
+
+    for (let i = 0; i < height; ++i) {
+      for (let j = 0; j < width; ++j) {
+        let index = (i * width + j) * 4;
+        out.data[index] = 255 - data[index];
+        out.data[index + 1] = 255 - data[index + 1];
+        out.data[index + 2] = 255 - data[index + 2];
+        out.data[index + 3] = data[index + 3];
+      }
+    }
+    return out;
   }
 
   function imageLoader(path, callback) {
