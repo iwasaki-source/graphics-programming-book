4a68ccc86d59782db886421a549fc0c9ed3b2aca
iwasaki <poettype73@gmail.com>
2024-12-26 00:43:46 +0900

    add 005

--- pixel/script/script.js
+++ pixel/script/script.js
@@ -26,9 +26,54 @@
 
     // let outputData = invertFilter(imageData);
     // let outputData = grayscaleFilter(imageData);
-    let outputData = binarizationFilter(imageData);
+    // let outputData = binarizationFilter(imageData);
+    let outputData = laplacianFilter(imageData);
 
     ctx.putImageData(outputData, 0, 0);
+  }
+
+  // ラプラシアンフィルター
+  function laplacianFilter(imageData) {
+    let width = imageData.width;
+    let height = imageData.height;
+    let data = imageData.data;
+
+    let out = ctx.createImageData(width, height);
+
+    for (let i = 0; i < height; ++i) {
+      for (let j = 0; j < width; ++j) {
+        let index = (i * width + j) * 4;
+
+        let topIndex = (Math.max(i - 1, 0) * width + j) * 4;
+        let bottomIndex = (Math.min(i + 1, height - 1) * width + j) * 4;
+        let leftIndex = (i * width + Math.max(j - 1, 0)) * 4;
+        let rightIndex = (i * width + Math.min(j + 1, width - 1)) * 4;
+
+        let r = data[topIndex] +
+                data[bottomIndex] +
+                data[leftIndex] +
+                data[rightIndex] +
+                data[index] * -4;
+        let g = data[topIndex + 1] +
+                data[bottomIndex + 1] +
+                data[leftIndex + 1] +
+                data[rightIndex + 1] +
+                data[index + 1] * -4;
+        let b = data[topIndex + 2] +
+                data[bottomIndex + 2] +
+                data[leftIndex + 2] +
+                data[rightIndex + 2] +
+                data[index + 2] * -4;
+
+        let value = (Math.abs(r) + Math.abs(g) + Math.abs(b)) / 3;
+
+        out.data[index] = value;
+        out.data[index + 1] = value;
+        out.data[index + 2] = value;
+        out.data[index + 3] = data[index + 3];
+      }
+    }
+    return out;
   }
 
   // 二値化フィルター
