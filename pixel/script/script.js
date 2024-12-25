7f3fde90a475642f52240cd6f8864c6e9dbede4f
iwasaki <poettype73@gmail.com>
2024-12-26 00:28:40 +0900

    add 004

--- pixel/script/script.js
+++ pixel/script/script.js
@@ -25,9 +25,38 @@
     let imageData = ctx.getImageData(0, 0, CANVAS_SIZE, CANVAS_SIZE);
 
     // let outputData = invertFilter(imageData);
-    let outputData = grayscaleFilter(imageData);
+    // let outputData = grayscaleFilter(imageData);
+    let outputData = binarizationFilter(imageData);
 
     ctx.putImageData(outputData, 0, 0);
+  }
+
+  // 二値化フィルター
+  function binarizationFilter(imageData) {
+    let width = imageData.width;
+    let height = imageData.height;
+    let data = imageData.data;
+
+    let out = ctx.createImageData(width, height);
+
+    for (let i = 0; i < height; ++i) {
+      for (let j = 0; j < width; ++j) {
+        let index = (i * width + j) * 4;
+
+        let r = data[index];
+        let g = data[index + 1];
+        let b = data[index + 2];
+
+        let luminace = (r + g + b) / 3;
+        let value = luminace >= 128 ? 255 : 0;
+
+        out.data[index] = value;
+        out.data[index + 1] = value;
+        out.data[index + 2] = value;
+        out.data[index + 3] = data[index + 3];
+      }
+    }
+    return out;
   }
 
   // グレイスケールフィルター
